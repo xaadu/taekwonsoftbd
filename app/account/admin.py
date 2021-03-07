0cd203c569f762fba975ede04e9a335f232e70d2
xaadu <xayed42@gmail.com>
2021-03-08 01:02:23 +0600

    Added JudgeModel on Main Admin Panel

--- app/account/admin.py
+++ app/account/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, TeamLeaderModel
+from .models import User, TeamLeaderModel, JudgeModel
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(TeamLeaderModel)
+admin.site.register(JudgeModel)
