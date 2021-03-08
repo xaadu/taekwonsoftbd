b54a7f1094b6ac4329825068f32cc0cb16296cd9
xaadu <xayed42@gmail.com>
2021-03-08 21:31:55 +0600

    Added Permission Control Decorator

--- app/account/decorators.py
+++ app/account/decorators.py
@@ -1,4 +1,7 @@
 from django.shortcuts import redirect
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import logout
 
 
 def unauthenticated_user(view_func):
@@ -8,3 +11,26 @@ def unauthenticated_user(view_func):
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
+
+
+def allowed_users(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper_func(request, *args, **kwrgs):
+            if request.user.is_authenticated:
+                if 'judge' in allowed_roles and request.user.is_judge:
+                    return view_func(request, *args, **kwrgs)
+                elif 'tl' in allowed_roles and request.user.is_tl:
+                    return view_func(request, *args, **kwrgs)
+                elif 'admin' in allowed_roles and request.user.is_superuser:
+                    return view_func(request, *args, **kwrgs)
+                else:
+                    logout(request)
+                    messages.error(
+                        request, 'You are not authorized to view the page. Please login with a permitted account.')
+                    return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+            else:
+                messages.error(
+                    request, 'You are not logged in. Login to view the page.')
+                return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+        return wrapper_func
+    return decorator
