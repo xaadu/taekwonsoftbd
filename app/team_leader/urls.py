a4ef8dc4d56349d4d2ad835fd185122fb201eaa3
xaadu <xayed42@gmail.com>
2021-03-09 05:15:05 +0600

    Fixed URL Patterns

--- app/team_leader/urls.py
+++ app/team_leader/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('players/', player_list, name='players'),
     path('teams/', team_list, name='teams'),
-    path('add_player/', add_player, name='add_player'),
-    path('add_team/', add_team, name='add_team'),
-    path('update_player/', update_player, name='update_player'),
-    path('update_team/', update_team, name='update_team'),
+    path('add-player/', add_player, name='add_player'),
+    path('add-team/', add_team, name='add_team'),
+    path('update-player/', update_player, name='update_player'),
+    path('update-team/', update_team, name='update_team'),
 ]
