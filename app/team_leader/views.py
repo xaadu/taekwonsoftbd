2c82fe36f6829ae6d1e70e474cd289ad41c0eed3
xaadu <xayed42@gmail.com>
2021-03-09 20:41:55 +0600

    Fixed Navbar Text

--- app/team_leader/views.py
+++ app/team_leader/views.py
@@ -4,13 +4,14 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 
 from account.models import TeamLeaderModel
+from account.decorators import allowed_users
 from .forms import PlayerCreateForm, TeamCreateForm, TLUpdateForm
 from .models import Player, Team
 
 
 # Create your views here.
 
-
+@allowed_users(['tl'])
 def dashboard(request):
     pic_url = request.user.get_prof_pic_url()
     players = request.user.teamleadermodel.player_set.all()
@@ -23,6 +24,7 @@ def dashboard(request):
     return render(request, 'team_leader/dashboard.html', context=context)
 
 
+@allowed_users(['tl'])
 def profile(request):
     pic_url = request.user.get_prof_pic_url()
     form = TLUpdateForm(instance=request.user, request=request)
@@ -46,6 +48,7 @@ def profile(request):
     return render(request, 'prof_update.html', context)
 
 
+@allowed_users(['tl'])
 def player_list(request):
     player_list = request.user.teamleadermodel.player_set.all()
     page = request.GET.get('page', 1)
@@ -59,6 +62,7 @@ def player_list(request):
     return render(request, 'team_leader/player_list.html', {'players': players})
 
 
+@allowed_users(['tl'])
 def team_list(request):
     team_list = request.user.teamleadermodel.team_set.all()
     page = request.GET.get('page', 1)
@@ -72,6 +76,7 @@ def team_list(request):
     return render(request, 'team_leader/team_list.html', {'teams': teams})
 
 
+@allowed_users(['tl'])
 def add_player(request):
     form = PlayerCreateForm()
 
@@ -92,6 +97,7 @@ def add_player(request):
     return render(request, 'team_leader/mod_data.html', context=context)
 
 
+@allowed_users(['tl'])
 def add_team(request):
     form = TeamCreateForm()
 
@@ -112,6 +118,7 @@ def add_team(request):
     return render(request, 'team_leader/mod_data.html', context=context)
 
 
+@allowed_users(['tl'])
 def update_player(request, pk):
     player = Player.objects.get(pk=pk)
     form = PlayerCreateForm(instance=player)
@@ -133,6 +140,7 @@ def update_player(request, pk):
     return render(request, 'team_leader/mod_data.html', context=context)
 
 
+@allowed_users(['tl'])
 def update_team(request, pk):
     team = Team.objects.get(pk=pk)
     form = TeamCreateForm(instance=team)
@@ -154,12 +162,14 @@ def update_team(request, pk):
     return render(request, 'team_leader/mod_data.html', context=context)
 
 
+@allowed_users(['tl'])
 def remove_player(request, pk):
     Player.objects.get(pk=pk).delete()
     messages.success(request, 'Player Removed Successfully.')
     return redirect('team_leader:players')
 
 
+@allowed_users(['tl'])
 def remove_team(request, pk):
     Team.objects.get(pk=pk).delete()
     messages.success(request, 'Team Removed Successfully.')
