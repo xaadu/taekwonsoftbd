908edd75e261eb2189347b494303fc1a88ab961c
xaadu <xayed42@gmail.com>
2021-03-10 14:36:42 +0600

    Added auto country code

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,3 +3,9 @@ $(function () {
         $(this).delay(2500).fadeOut();
     });
 });
+$("#id_telephone").on("blur keyup change", function () {
+    if ($(this).val() == '') {
+        var getCode = $("#id_telephone").intlTelInput('getSelectedCountryData').dialCode;
+        $(this).val('+' + getCode);
+    }
+});
