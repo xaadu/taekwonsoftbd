2def4ce324b21d58fd28d1fdf0c7e5579fb0456c
xaadu <xayed42@gmail.com>
2021-04-19 11:15:28 +0600

    Added Social Section

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -18,3 +18,46 @@ $("#id_telephone").on("blur keyup change", function () {
         }
     }
 });
+
+$(document).ready(function () {
+    $('.socialSlider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
