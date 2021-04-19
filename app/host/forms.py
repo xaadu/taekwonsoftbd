fd42b400c35643b6a16a5f2ae0619a27f031caeb
xaadu <xayed42@gmail.com>
2021-04-19 06:33:53 +0600

    Refreshed Labels in Event Create

--- app/host/forms.py
+++ app/host/forms.py
@@ -26,6 +26,29 @@ class EventCreateForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].label = 'Event Title'
+        self.fields['event_banner'].label = 'Event Banner Image'
+        self.fields['event_date'].label = 'Event Date'
+        self.fields['reg_deadline'].label = 'Registration Deadline'
+
+        self.fields['judge_1'].label = 'Main Judge'
+        self.fields['judge_2'].label = 'First Judge'
+        self.fields['judge_3'].label = 'Second Judge'
+
+        self.fields['allow_reg'].label = 'Allow Registration'
+        self.fields['completed'].label = 'Event Completed'
+
+        # self.fields[''].label = ''
+
+        for key, value in self.fields.items():
+            label = value.label
+            if value.required:
+                label = label+'*'
+                self.fields[key].label = label
 
 
 class CategoryCreateForm(forms.ModelForm):
