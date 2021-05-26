c99ffa2d7d0c7718771381ea91fccfed59b0312d
Abdullah Zayed <xayed42@gmail.com>
2021-05-26 17:31:43 +0600

    Added date in Renew Log

--- proxy/shell/renew.sh
+++ proxy/shell/renew.sh
@@ -2,6 +2,6 @@ text=$(certbot renew --post-hook "nginx -s reload")
 
 /home/backup.sh
 
-now=$(date +"%r")
+now="$(date +'%d/%m/%Y') $(date +"%r")"
 
 curl -X POST -d "log=$now: $text" http://taekwonsoftbdlog.zayedabdullah.com/
