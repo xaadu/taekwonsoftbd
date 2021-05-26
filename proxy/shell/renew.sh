f9e6e1008e1e0e7d5a542937126236f96d8291ca
Abdullah Zayed <xayed42@gmail.com>
2021-05-26 17:48:20 +0600

    Updated Log Style

--- proxy/shell/renew.sh
+++ proxy/shell/renew.sh
@@ -4,4 +4,5 @@ text=$(certbot renew --post-hook "nginx -s reload")
 
 now="$(date +'%d/%m/%Y') $(date +"%r")"
 
-curl -X POST -d "log=$now: $text" http://taekwonsoftbdlog.zayedabdullah.com/
+curl -X POST -d "log=$now
+$text" http://taekwonsoftbdlog.zayedabdullah.com/
