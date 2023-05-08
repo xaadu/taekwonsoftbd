61d62f1ff1a263ed89181c728eb05164880c0406
xaadu <abdullahzayed01@gmail.com>
2023-05-09 03:54:13 +0600

    [ UPDATED ] password reset form for user in admin

--- app/account/admin.py
+++ app/account/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User, TeamLeaderModel, JudgeModel
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(TeamLeaderModel)
 admin.site.register(JudgeModel)
